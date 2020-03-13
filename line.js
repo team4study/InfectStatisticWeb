35357cd3bbaa6011cd5c564155836bf7faee9914
maoyudun <598360870@qq.com>
2020-03-13 19:10:42 +0800

    将折线图模型整合进主页面

---
+++ line.js
@@ -0,0 +1,86 @@
+function init1() {
+    echartsLine(0);
+}
+
+function echartsLine(x) {
+    var myChart = echarts.init(document.getElementById('global-line'));
+
+    var option = {
+        tooltip: {
+            trigger: 'axis'
+        },
+        toolbox: {
+            show: true,
+            feature: {
+                mark: {
+                    show: true
+                },
+                dataView: {
+                    show: true,
+                    readOnly: false
+                },
+                magicType: {
+                    show: true,
+                    type: ['line', 'bar', 'stack', 'tiled']
+                },
+                restore: {
+                    show: true
+                },
+                saveAsImage: {
+                    show: true
+                }
+            }
+        },
+        calculable: true,
+        xAxis: [{
+            type: 'category',
+            boundaryGap: false,
+            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+        }],
+        yAxis: [{
+            type: 'value'
+        }],
+        series: [{
+                name: '类型1',
+                type: 'line',
+                stack: '总量',
+                data: [120, 132, 101, 134, 90, 230, 210]
+            },
+            {
+                name: '类型2',
+                type: 'line',
+                stack: '总量',
+                data: [220, 182, 191, 234, 290, 330, 310]
+            },
+            {
+                name: '类型3',
+                type: 'line',
+                stack: '总量',
+                data: [150, 232, 201, 154, 190, 330, 410]
+            },
+            {
+                name: '类型4',
+                type: 'line',
+                stack: '总量',
+                data: [320, 332, 301, 334, 390, 330, 320]
+            },
+            {
+                name: '类型5',
+                type: 'line',
+                stack: '总量',
+                data: [820, 932, 901, 934, 1290, 1330, 1320]
+            }
+        ],
+        legend: {
+            data: ['类型1', '类型2', '类型3', '类型4', '类型5']
+        }
+    };
+
+    // 为echarts对象加载数据 
+    myChart.setOption(option);
+}
+window.onload = function() {
+    this.init1();
+    this.init2();
+
+}
